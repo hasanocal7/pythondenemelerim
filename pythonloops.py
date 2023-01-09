4e50893f48be5fcb371f63c5c18ae301f010924c
Hasan Ocal <hasanocal7@gmail.com>
2023-01-09 14:23:03 +0300

    Denemeler

---
+++ pythonloops.py
@@ -0,0 +1,14 @@
+"""for i in range(10):
+    print(i)
+j=1
+while j<10:
+    print(j)
+    j+=1"""
+list=["ahmet","mehmet","ciklet"]
+for i in list:
+    print(i)
+for i in range(1,20):
+    if i==15:
+        continue
+    print(i)
+    
---
+++ pythonndenemeler.py
@@ -0,0 +1,9 @@
+"""ad="hasan"
+soyad="ocal"
+numara="121620191020"
+sayi=1
+sozluk={'ad':'hasan','soyad':'ocal'}"""
+i=1
+j=2
+k="3"
+print(i+j+int(k))
