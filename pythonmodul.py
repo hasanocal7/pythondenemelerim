3c44c61d55242995a86e34710f2bfe8e5fdeffe1
Hasan Ocal <hasanocal7@gmail.com>
2023-01-09 16:04:56 +0300

    random

--- pythonmodul.py
+++ pythonmodul.py
@@ -1,7 +1,7 @@
 """import math
 print(math.__doc__)"""
 import random
-x=random.randrange(100)
+"""x=random.randrange(100)
 while True:
     sayi=int(input("Sayı giriniz: "))
     if sayi==x:
@@ -10,4 +10,16 @@ while True:
     elif sayi<x:
         print("Az")
     else:
-        print("Fazla")    
+        print("Fazla")   """
+
+
+
+
+kursun=random.randrange(1,7)
+for i in range(1,7):
+    if i!=kursun:
+        _=input("SIK")
+        continue
+    else:
+        print("ÖLDÜN")
+        break
